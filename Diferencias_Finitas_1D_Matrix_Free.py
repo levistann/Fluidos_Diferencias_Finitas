db3ce385f9ff83d7a74e872acf2369ecb5983207
levi <levistan9@gmail.com>
2026-03-14 07:15:42 -0600

    Metodo Matrix-Free

--- Diferencias_Finitas_1D_Matrix_Free.py
+++ Diferencias_Finitas_1D_Matrix_Free.py
@@ -5,65 +5,43 @@ import matplotlib.pyplot as plt
 
 #funtos de inicio y final
 x_start = 0.0
-x_final = 1
-n_points = 5 #numero de puntos a aproximar
+x_final = 20
+n_points = 200 #numero de puntos a aproximar
 
 
 #condiciones de frontera
-a = 2*x_start
-b = 2*x_final
+a = np.cos(x_start)
+b = np.cos(x_final)
 
 step = (x_final - x_start)/(n_points - 1)    #paso
 x = np.linspace(x_start, x_final, n_points)     #dominio de nuestra funcion
 def u(x):       #definimos la funcion de la cual aproximaremos du/dx
-    return x**2
+    return np.sin(x)
 
 U = u(x)    #evaluamos nuestra funcion sobre el dominio
 
-
 #Operador hacia atras
-#matriz de coeficientes
-D_left = np.diag(np.ones(n_points)/step) + np.diag(-np.ones(n_points - 1)/step, k = -1)
-#ajustamos la matriz para las condiciones iniciales
-D_left[0, :] = 0
-D_left[-1, :] = 0
-#calculamos la derivada aproximada e inyectamos las condiciones iniciales
-u_prime_left = D_left @ U
+u_prime_left = np.zeros_like(U)
+u_prime_left[1:] = (U[1:] - U[:-1])/step
 u_prime_left[0] = a
-u_prime_left[-1] = b
-
 
 
 #Operador hacia adelante
-#matriz de coeficientes
-D_right = np.diag(np.ones(n_points - 1)/step, k = 1) + np.diag(-np.ones(n_points)/step)
-#ajustamos la matriz para las condiciones iniciales
-D_right[0, :] = 0
-D_right[-1, :] = 0
-#calculamos la derivada aproximada e inyectamos las condiciones iniciales
-u_prime_right = D_right @ U
-u_prime_right[0] = a
+u_prime_right = np.zeros_like(U)
+u_prime_right[:-1] = (U[1:] - U[:-1])/step
 u_prime_right[-1] = b
 
 
-
 #Operador central
-#matriz de coeficientes
-D_0 = np.diag(np.ones(n_points - 1)/(2*step), k = 1) + np.diag(-np.ones(n_points - 1)/(2*step), k = -1)
-#ajustamos la matriz para las condiciones iniciales
-D_0[0, :] = 0
-D_0[-1, :] = 0
-#calculamos la derivada aproximada e inyectamos las condiciones iniciales
-u_prime_central = D_0 @ U
+u_prime_central = np.zeros_like(U)
+u_prime_central[1:-1] = (U[2:] - U[:-2])/(2*step)
 u_prime_central[0] = a
 u_prime_central[-1] = b
 
-
-
-
 #solucion analitica
-u_exact = 2*x
+u_exact = np.cos(x)
 
+'''
 fig1, (ax1, ax2) = plt.subplots(1, 2, figsize = (14, 5))
 ax1.plot(x, u_exact, 'k-', label = 'Solucion Analitica')
 ax1.plot(x, u_prime_left, 'bo-', label = r'Solucion aproximada por  $D_{-u(x)}$')
@@ -76,7 +54,6 @@ ax1.grid(True)
 ax2.plot(x, u_exact, 'k-', label = 'Solucion Analitica')
 ax2.plot(x, u_prime_right, 'bo-', label = r'Solucion aproximada por  $D_{+u(x)}$')
 ax2.set_xlabel('valor de x')
-#ax2.ylabel('valor de la funcion')
 ax2.set_title(r"Comparacion solucion analitica vs aproximada de:   $\frac{dsin(x)}{dx}$  con  $D_{+u(x)}$")
 ax2.legend()
 ax2.grid(True)
@@ -87,7 +64,17 @@ ax3.plot(x, u_exact, 'k-', label = 'Solucion Analitica')
 ax3.plot(x, u_prime_central, 'bo-', label = r'Solucion aproximada por  $D_{0u(x)}$')
 ax3.set_xlabel('valor de x')
 ax3.set_ylabel('valor de la funcion')
-ax3.set_title(r"Comparacion solucion analitica vs aproximada de:   $\frac{dsin(x)}{dx}$  con  $D_{0u(x)}$")
+ax3.set_title(r"Comparacion solucion analitica vs aproximada de:   $\frac{dsin(x)}{dx}$  con  $D_{0u(x)}$" + f"y h = {step}")
 ax3.legend()
 ax3.grid(True)
 plt.show()
+'''
+
+#Extraer el error
+error = u_prime_central - u_exact
+
+L_inf = np.max(np.abs(error))
+L_sqrd = np.sqrt(step*np.sum(error**2))
+
+print(L_inf)
+print(L_sqrd)
